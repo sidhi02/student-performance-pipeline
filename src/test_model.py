259ecf1c835e1a1b98cb02c7219dcaf06c8efed0
sidhi02 <sidhideshmukh32@gmail.com>
2026-04-17 00:31:49 +0530

    Fix tests to pytest format

--- src/test_model.py
+++ src/test_model.py
@@ -1,7 +1,18 @@
-import unittest
 import os
 import sys
 
 sys.path.append("src")
 
 from train import train_model
+
+def test_training():
+    score = train_model()
+    assert score is not None
+
+def test_performance():
+    score = train_model()
+    assert score > 0.0
+
+def test_model_saved():
+    train_model()
+    assert os.path.exists("models/model.pkl")
